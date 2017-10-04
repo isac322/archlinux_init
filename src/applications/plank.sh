a7ad29b14343060d633ae489361fcc4b3ac4baab
isac322 <bh322yoo@gmail.com>
2017-10-05 00:34:15 +0900

    add plank configuration script

---
+++ src/applications/plank.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+dconf write /net/launchpad/plank/docks/dock1/icon-size 66
+dconf write /net/launchpad/plank/docks/dock1/theme 'Transparent'
+dconf write /net/launchpad/plank/docks/dock1/zoom-enabled true
+dconf write /net/launchpad/plank/docks/dock1/zoom-percent 164
