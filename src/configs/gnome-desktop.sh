7949270f10495a80376f48747e68181513222531
Byeonghoon Yoo <bh322yoo@gmail.com>
2018-06-03 14:45:21 +0900

    update dconf files

--- src/configs/gnome-desktop.sh
+++ src/configs/gnome-desktop.sh
@@ -5,6 +5,8 @@ BASEDIR=`dirname "$SCRIPT"`
 
 dconf load /org/gnome/desktop/ < ${BASEDIR}/gnome-desktop.conf
 
+sudo cp -av /usr/share/gnome-shell/gnome-shell-theme.gresource{,~}
 
 GTK_THEME=$(gsettings get org.gnome.desktop.interface gtk-theme | sed "s/'//g")
-sudo cp --backup /usr/share{/themes/${GTK_THEME},}/gnome-shell/gnome-shell-theme.gresource
+cd /usr/share/themes/${GTK_THEME}/gnome-shell
+sudo glib-compile-resources --target=/usr/share/gnome-shell/gnome-shell-theme.gresource gnome-shell-theme.gresource.xml
