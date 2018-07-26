d689daf9b614b9836eb863e43d19eef08adf94a0
Byeonghoon Yoo <bh322yoo@gmail.com>
2018-07-26 22:44:13 +0900

    fix errors

--- src/environment.sh
+++ src/environment.sh
@@ -105,6 +105,7 @@ tee /etc/X11/xinit/xinitrc.d/99-trackpoint-scroll.sh > /dev/null << END
 xinput set-prop "DELL081C:00 044E:121F Mouse" "libinput Scroll Method Enabled" 0 0 1
 xinput set-prop "DELL081C:00 044E:121F Mouse" "libinput Accel Speed" -0.4
 END
+chmod +x /etc/X11/xinit/xinitrc.d/99-trackpoint-scroll.sh
 
 # install desktop
 pacman -S baobab eog eog-plugins evince gdm gnome-calculator gnome-control-center gnome-disk-utility gnome-font-viewer \
@@ -112,7 +113,7 @@ pacman -S baobab eog eog-plugins evince gdm gnome-calculator gnome-control-cente
  gucharmap gvfs gvfs-afc gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb mousetweaks nautilus \
  networkmanager totem gst-libav vino xdg-user-dirs-gtk cheese dconf-editor file-roller gedit gnome-logs gnome-mines \
  gnome-sound-recorder gnome-tweak-tool gnome-video-effects seahorse vinagre gparted meld ttf-ubuntu-font-family \
- fcitx-configtool fcitx-hangul fcitx-gtk3 --noconfirm
+ fcitx-configtool fcitx-hangul fcitx-gtk3 xorg-xinput --noconfirm
 
 
 # for printer support
